706a99f820c0e99660890a6e08ac64c03cab0959
Steve Hoeffner <hoeffner3@gmail.com>
2013-04-01 20:05:59 -0400

    Added mohak's class and layout

---
+++ src/SE_spring2013_g8/hal/Climate/ClimateControl.java
@@ -0,0 +1,18 @@
+package SE_spring2013_g8.hal.Climate;
+
+import SE_spring2013_g8.hal.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class ClimateControl extends Activity {
+	
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.climate_control);
+	}
+
+}
