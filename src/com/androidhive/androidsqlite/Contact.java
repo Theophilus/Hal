d01decc7447d3fcd1bbd3c1e01d12e48703b959b
Herrick Ching <hching1992@hotmail.com>
2013-05-09 20:12:31 -0400

    Also, pulling when everyone else is working is a bitch

---
+++ src/com/androidhive/androidsqlite/Contact.java
@@ -0,0 +1,55 @@
+package com.androidhive.androidsqlite;
+
+public class Contact {
+	
+	//private variables
+	int _id;
+	String _name;
+	String IP_Address;
+	
+	// Empty constructor
+	public Contact(){
+		
+	}
+	// constructor
+	public Contact(int id, String name, String IP_Address){
+		this._id = id;
+		this._name = name;
+		this.IP_Address = IP_Address;
+	}
+	
+	// constructor
+	public Contact(String name, String IP_Address){
+		this._name = name;
+		this.IP_Address = IP_Address;
+	}
+	// getting ID
+	public int getID(){
+		return this._id;
+	}
+	
+	// setting id
+	public void setID(int id){
+		this._id = id;
+	}
+	
+	// getting name
+	public String getName(){
+		return this._name;
+	}
+	
+	// setting name
+	public void setName(String name){
+		this._name = name;
+	}
+	
+	// getting phone number
+	public String getIPAddress(){
+		return this.IP_Address;
+	}
+	
+	// setting phone number
+	public void setPhoneNumber(String phone_number){
+		this.IP_Address = phone_number;
+	}
+}
